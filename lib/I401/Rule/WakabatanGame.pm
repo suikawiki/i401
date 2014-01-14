dcf5ae2189dd7998db4ddf6ef0e5ab870dc48efe
pokutuna <popopopopokutuna@gmail.com>
2014-01-14 22:02:39 +0900

    わかばたんゲーム

---
+++ lib/I401/Rule/WakabatanGame.pm
@@ -0,0 +1,25 @@
+package I401::Rule::WakabatanGame;
+use strict;
+use warnings;
+use utf8;
+
+my $string = 'わかばたん';
+my $channel_sequences = {};
+
+sub get {
+    return ({
+        privmsg => 1,
+        pattern => qr{.},
+        code => sub {
+            my ($irc, $args) = @_;
+            my $seq = $channel_sequences->{$args->{channel}} //= [ split '', $string ];
+            if ($seq->[0] eq $args->{text}) {
+                push $seq, shift $seq;
+                $irc->send_notice($args->{channel}, $seq->[0]);
+                push $seq, shift $seq;
+            }
+        }
+    });
+}
+
+1;
