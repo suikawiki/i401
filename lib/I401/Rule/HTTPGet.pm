8f7d96f671917927dff69cbdee987129443bb490
Wakaba <wakabatan@hatena.ne.jp>
2014-01-16 16:33:41 +0900

    header dumper; legacy japanese charset support

--- lib/I401/Rule/HTTPGet.pm
+++ lib/I401/Rule/HTTPGet.pm
@@ -4,34 +4,77 @@ use warnings;
 use Time::HiRes qw(time);
 use Encode;
 use AnyEvent::HTTP;
+use AnyEvent::Util qw(run_cmd);
 
 sub get {
     return ({
         privmsg => 1,
-        pattern => qr<(https?://[0-9A-Za-z_,\$!&:();./?+%\@=#-]+)>,
+        pattern => qr<(?:(header)\s+)?(https?://[0-9A-Za-z_,\$!&:();./?+%\@=#-]+)>,
         code => sub {
             my ($irc, $args) = @_;
-
-            $irc->log("Get <$1>");
-            my $start_time = time;
-            http_get $1, sub {
-                my ($data, $headers) = @_;
-                $data = '' unless defined $data;
-                my $title = '';
-                if ($data =~ m{<title>(.*?)</title>}s) {
-                    $title = decode 'utf-8', $1;
-                }
-                my $msg = sprintf '[%d %s, %s %.3f KB %.3f s] %s',
-                    $headers->{Status},
-                    (decode 'utf-8', $headers->{Reason}),
-                    (defined $headers->{'content-type'}
-                         ? $headers->{'content-type'} : '(no Content-Type)'),
-                    (length $data) / 1024,
-                    time - $start_time,
-                    $title;
-                $irc->send_notice($args->{channel}, $msg);
-            };
+            my $mode = 'process_' . ($1 || 'default');
+            my $url = $2;
+            __PACKAGE__->$mode($irc, $args, $url);
         },
+    });
+}
+
+sub process_default {
+    my ($class, $irc, $args, $url) = @_;
+
+    $irc->log("Get <$url>");
+    my $start_time = time;
+    http_get $url, sub {
+        my ($data, $headers) = @_;
+        $data = '' unless defined $data;
+        my $title = '';
+        my $charset = 'utf-8';
+        if (($headers->{'content-type'} || '') =~ /charset=(\S+)/) {
+            $charset = {'euc-jp' => 'euc-jp',
+                        'shift_jis' => 'windows-31j'}->{lc $1} || $charset;
+        }
+        if ($data =~ m{<title>(.*?)</title>}s) {
+            $title = decode $charset, $1;
+        }
+        my $msg = sprintf '[%d %s, %s %.3f KB %.3f s] %s',
+            $headers->{Status},
+            (decode 'utf-8', $headers->{Reason}),
+            (defined $headers->{'content-type'}
+                 ? $headers->{'content-type'} : '(no Content-Type)'),
+            (length $data) / 1024,
+            time - $start_time,
+            $title;
+        $irc->send_notice($args->{channel}, $msg);
+    };
+}
+
+sub process_header {
+    my ($class, $irc, $args, $url) = @_;
+
+    my $pid;
+    my $in_header = 1;
+    my $header = [];
+    $irc->log("Get <$url>");
+    run_cmd(
+        ['curl', '-D', '-', $url],
+        '>' => sub {
+            return unless $in_header;
+            my $data = shift;
+            if ($data =~ /\x0D\x0A\x0D\x0A/) {
+                push @$header, [split /\x0D\x0A\x0D\x0A/, $data, 2]->[0];
+                kill 'INT', $pid;
+                undef $in_header;
+            } else {
+                push @$header, $data;
+            }
+        },
+        '$$' => \$pid,
+    )->cb(sub {
+        my $msg = join '', @$header;
+        for (split /\x0D\x0A/, $msg) {
+            tr/\x0D\x0A/  /;
+            $irc->send_notice($args->{channel}, $_);
+        }
     });
 }
 
