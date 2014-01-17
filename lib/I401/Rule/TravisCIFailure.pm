573a405c76cbac1398769c25f112164a564bec27
Wakaba <wakabatan@hatena.ne.jp>
2014-01-17 17:58:27 +0900

    error

--- lib/I401/Rule/TravisCIFailure.pm
+++ lib/I401/Rule/TravisCIFailure.pm
@@ -5,7 +5,7 @@ use warnings;
 sub get {
     return ({
         notice => 1,
-        pattern => qr<(The build.*?fail(?:ed|ing)\.)>,
+        pattern => qr<(The build.*?(?:fail|error)(?:ed|ing)\.)>,
         code => sub {
             my ($irc, $args) = @_;
             $irc->send_privmsg($args->{channel}, $1);
