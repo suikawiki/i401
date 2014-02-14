eadaa5b0b516a00b1d8f73f7cc89f44680fb1898
pokutuna <popopopopokutuna@gmail.com>
2014-02-14 17:01:39 +0900

    '>' で呼びかけない場合は notice で指名する & インデント2にあわせる

--- lib/I401/Rule/Anybody.pm
+++ lib/I401/Rule/Anybody.pm
@@ -4,26 +4,26 @@ use warnings;
 use utf8;
 
 my $BeggingText = [
-    'お願いします',
-    'よろすく',
-    'まかせた',
-    '頼む',
-    'おにゃーしゃー',
-    'じゃあそれで',
+  'お願いします',
+  'よろすく',
+  'まかせた',
+  '頼む',
+  'おにゃーしゃー',
+  'じゃあそれで',
 ];
 
 sub get ($) {
-    return ({
-        privmsg => 1,
-        pattern => qr{(?:誰|だれ)か},
-        code    => sub {
-            my ($irc, $args) = @_;
-            my $users = $irc->get_channel_users($args->{channel});
-            my $anybody = $users->[int(rand(@$users))];
-            my $begging = $BeggingText->[int(rand(@$BeggingText))];
-            $irc->send_privmsg($args->{channel}, join(' > ', $begging, $anybody));
-        },
-    });
+  return ({
+    privmsg => 1,
+    pattern => qr{>?\s?(?:誰|だれ)か},
+    code    => sub {
+      my ($irc, $args) = @_;
+      my $users = $irc->get_channel_users($args->{channel});
+      my $msg = join(' > ', $BeggingText->[int(rand(@$BeggingText))], $users->[int(rand(@$users))]);
+      my $method = $args->{text} =~ />\s?(?:誰|だれ)か/ ? 'send_privmsg' : 'send_notice';
+      $irc->$method($args->{channel}, $msg);
+    },
+  });
 }
 
 1;
