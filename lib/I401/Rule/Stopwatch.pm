ebb036eb6347c7d85fa2b256f871e5d5530dfeaf
pokutuna <popopopopokutuna@gmail.com>
2014-02-14 12:22:58 +0900

    Stopwatch 途中経過おしえて機能 & チャンネルごとに timer 分ける

--- lib/I401/Rule/Stopwatch.pm
+++ lib/I401/Rule/Stopwatch.pm
@@ -3,6 +3,7 @@ use strict;
 use warnings;
 use utf8;
 
+# { channel => { name => start_time, ... }, ... }
 my $StartTimes = {};
 
 sub get ($) {
@@ -14,7 +15,7 @@ sub get ($) {
       my $name = $1;
       $name =~ s/\A\s+//;
       $name =~ s/\s+\z//;
-      $StartTimes->{$name} = time;
+      ($StartTimes->{$args->{channel}} ||= {})->{$name} = time;
       $irc->send_notice($args->{channel}, "$name\開始なう");
     },
   }, {
@@ -25,11 +26,28 @@ sub get ($) {
       my $name = $1;
       $name =~ s/\A\s+//;
       $name =~ s/\s+\z//;
-      if (defined $StartTimes->{$name}) {
-        my $time = time - $StartTimes->{$name};
+      my $channel_times = ($StartTimes->{$args->{channel}} || {});
+      if (defined $channel_times->{$name}) {
+        my $time = time - $channel_times->{$name};
         $irc->send_notice($args->{channel}, "$name\開始から $time s でした");
+        delete $channel_times->{$name};
       }
     },
+  }, {
+    privmsg => 1,
+    pattern => qr{途中経過(?:教|おし)えて},
+    code => sub {
+      my ($irc, $args) = @_;
+      my $channel_times = ($StartTimes->{$args->{channel}} || {});
+      if (keys %$channel_times) {
+        my $lapse_msg = join(', ', map {
+            sprintf('%s: %s s', $_, time - $channel_times->{$_});
+        } keys %$channel_times);
+        $irc->send_notice($args->{channel}, $lapse_msg . ' だよ');
+      } else {
+        $irc->send_notice($args->{channel}, 'なんもないです');
+      }
+     },
   });
 }
 
