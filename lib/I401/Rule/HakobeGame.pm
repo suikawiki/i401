b6c0205a981bdce2ac4aa130f4120ca44f77cbce
Wakaba <wakabatan@hatena.ne.jp>
2014-03-18 17:33:43 +0900

    hakobe

---
+++ lib/I401/Rule/HakobeGame.pm
@@ -0,0 +1,20 @@
+package I401::Rule::HakobeGame;
+use strict;
+use warnings;
+use utf8;
+
+sub get ($) {
+  return ({
+    privmsg => 1,
+    pattern => qr{^([はこべ])っ([!！?？]?)$},
+    code => sub {
+      my ($irc, $args) = @_;
+      my $msg = {は => 'こ', こ => 'べ', べ => 'は'}->{$1};
+      $msg .= 'っ';
+      $msg .= {'!' => '!', '！' => '!', '?' => '?', '？' => '?'}->{$2} // '';
+      $irc->send_notice($args->{channel}, $msg);
+    },
+  });
+} # get
+
+1;
