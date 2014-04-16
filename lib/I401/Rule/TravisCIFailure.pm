ef1f03fd8d17ca29d6e059645f3c1875ed031cfc
Wakaba <wakaba@suikawiki.org>
2014-04-16 15:48:00 +0900

    more travis pattern

--- lib/I401/Rule/TravisCIFailure.pm
+++ lib/I401/Rule/TravisCIFailure.pm
@@ -2,15 +2,24 @@ package I401::Rule::TravisCIFailure;
 use strict;
 use warnings;
 
-sub get {
-    return ({
-        notice => 1,
-        pattern => qr<(The build.*?(?:fail|error)(?:ed|ing)\.)>,
-        code => sub {
-            my ($irc, $args) = @_;
-            $irc->send_privmsg($args->{channel}, $1);
-        },
-    });
-}
+sub get ($) {
+  return ({
+    notice => 1,
+    pattern => qr<(The build.*?(?:(?:fail|error)(?:ed|ing)|broken)\.)>,
+    code => sub {
+      my ($irc, $args) = @_;
+      $irc->send_privmsg ($args->{channel}, $1);
+    },
+  });
+} # get
 
 1;
+
+=head1 LICENSE
+
+Copyright 2014 Wakaba <wakaba@suikawiki.org>.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
