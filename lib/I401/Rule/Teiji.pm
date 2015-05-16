589698100f835732f7af7b7a9cc573e08eb2ee60
Wakaba <wakaba@suikawiki.org>
2015-05-16 10:38:13 +0900

    leap seconds

--- lib/I401/Rule/Teiji.pm
+++ lib/I401/Rule/Teiji.pm
@@ -4,6 +4,7 @@ use warnings;
 use utf8;
 use Time::Local qw(timegm_nocheck);
 use AnyEvent;
+use I401::Data::RemoteJSON;
 
 my $Events = {};
 
@@ -11,6 +12,27 @@ sub add_event ($$%) {
   my ($class, $name, %args) = @_;
   $Events->{$name} = \%args;
 } # add_event
+
+sub load_leap_data ($) {
+  my $code = $_[0];
+  I401::Data::RemoteJSON->get (q<https://raw.githubusercontent.com/manakai/data-locale/master/data/datetime/seconds.json>, sub {
+    my $data = shift;
+    if (ref $data eq 'HASH' and
+        ref $data->{positive_leap_seconds} eq 'HASH') {
+      my $time;
+      my $now = time;
+      for (values %{$data->{positive_leap_seconds}}) {
+        next unless ref $_ eq 'HASH';
+        $time ||= $_->{next_unix} if $_->{next_unix} > $now;
+      }
+      if (defined $time) {
+        __PACKAGE__->add_event (閏秒 => unix => $time);
+        __PACKAGE__->add_event (うるう秒 => unix => $time);
+      }
+    }
+    $code->();
+  }, max_age => 60*60*24*10);
+} # load_leap_data
 
 my $Timers = {};
 
@@ -34,48 +56,56 @@ sub get ($) {
       my $def;
       my $name = $1;
       $name =~ s/\s+\z//;
-      for (keys %$Events) {
-        if ($name =~ /\Q$_\E\z/) {
-          $def = $Events->{$_};
-          last;
-        }
+      my $cv = AE::cv;
+      if ($name eq '閏秒' or $name eq 'うるう秒') {
+        load_leap_data (sub { $cv->send });
+      } else {
+        $cv->send;
       }
-      return unless defined $def;
+      $cv->cb (sub {
+        for (keys %$Events) {
+          if ($name =~ /\Q$_\E\z/) {
+            $def = $Events->{$_};
+            last;
+          }
+        }
+        return unless defined $def;
 
-      my @time = gmtime;
-      $time[0] = $def->{second} || 0;
-      $time[1] = $def->{minute} if defined $def->{minute};
-      $time[2] = $def->{hour} if defined $def->{hour};
-      $time[3] = $def->{day} if defined $def->{day};
-      $time[4] = $def->{month}-1 if defined $def->{month};
-      $time[5] = $def->{year} if defined $def->{year};
-      my $step = $def->{step} || 'day';
-      my $time = timegm_nocheck (@time);
-      while ($time < time) {
-        $time[3]++ if $step eq 'day';
-        $time[4]++ if $step eq 'month';
-        $time[5]++ if $step eq 'year';
-        $time = timegm_nocheck (@time);
-      }
+        my @time = defined $def->{unix} ? gmtime $def->{unix} : gmtime;
+        $time[0] = $def->{second} || 0;
+        $time[1] = $def->{minute} if defined $def->{minute};
+        $time[2] = $def->{hour} if defined $def->{hour};
+        $time[3] = $def->{day} if defined $def->{day};
+        $time[4] = $def->{month}-1 if defined $def->{month};
+        $time[5] = $def->{year} if defined $def->{year};
+        my $step = $def->{step} || 'day';
+        my $time = timegm_nocheck (@time);
+        while ($time < time) {
+          $time[3]++ if $step eq 'day';
+          $time[4]++ if $step eq 'month';
+          $time[5]++ if $step eq 'year';
+          $time = timegm_nocheck (@time);
+        }
 
-      if ($step eq 'year') {
-        for (1..32, 50, 100, 200, 300) {
-          schedule $irc, $args->{channel}, "$name\まであと $_ 日", $time - $_*60*60*24;
+        if ($step eq 'year') {
+          for (1..32, 50, 100, 200, 300) {
+            schedule $irc, $args->{channel}, "$name\まであと $_ 日", $time - $_*60*60*24;
+          }
+          for (1..5, 10, 12, 20, 30, 50, 100) {
+            schedule $irc, $args->{channel}, "$name\まであと $_ 時間くらい", $time - $_*60*60;
+          }
+          for (1, 5, 10, 15, 20, 25, 30, 45, 100) {
+            schedule $irc, $args->{channel}, "$name\まであと $_ 分くらい", $time - $_*60;
+          }
+          for (1..10, 15, 30, 25, 40, 45, 50, 55, 100) {
+            schedule $irc, $args->{channel}, "$name\まであと $_ 秒くらい", $time - $_;
+          }
         }
-        for (1..5, 10, 12, 20, 30, 50, 100) {
-          schedule $irc, $args->{channel}, "$name\まであと $_ 時間くらい", $time - $_*60*60;
-        }
-        for (1, 5, 10, 15, 20, 25, 30, 45, 100) {
-          schedule $irc, $args->{channel}, "$name\まであと $_ 分くらい", $time - $_*60;
-        }
-        for (1..10, 15, 30, 25, 40, 45, 50, 55, 100) {
-          schedule $irc, $args->{channel}, "$name\まであと $_ 秒くらい", $time - $_;
-        }
-      }
-      schedule $irc, $args->{channel}, "$name\なう", $time;
+        schedule $irc, $args->{channel}, "$name\なう", $time;
 
-      my $seconds = $time - time;
-      $irc->send_notice($args->{channel}, "$name\まであと $seconds 秒くらい");
+        my $seconds = $time - time;
+        $irc->send_notice($args->{channel}, "$name\まであと $seconds 秒くらい");
+      });
     },
   }, {
     privmsg => 1,
