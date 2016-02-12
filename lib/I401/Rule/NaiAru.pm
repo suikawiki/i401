b6d4665e257677b82388b8d833057a0e3bb2bc32
Wakaba <wakaba@suikawiki.org>
2016-02-12 16:07:34 +0900

    naiaru

---
+++ lib/I401/Rule/NaiAru.pm
@@ -0,0 +1,28 @@
+package I401::Rule::NaiAru;
+use strict;
+use warnings;
+use utf8;
+
+my @suffix = ('', qw(
+  な
+  なぁ
+  な!!
+  wwwww
+  !!!!
+  〜〜〜!!!!
+));
+
+sub get ($) {
+  return ({
+    privmsg => 1,
+    pattern => qr{(\w+)ない[?？]*$},
+    code => sub {
+      my ($irc, $args) = @_;
+      my $msg = "$1ある";
+      $msg .= $suffix[rand @suffix];
+      $irc->send_privmsg ($args->{channel}, $msg);
+    },
+  });
+}
+
+1;
